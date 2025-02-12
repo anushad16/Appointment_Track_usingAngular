40f3f85c5d992f7a5ce1effeb214a9eeb33f1be1
anushaanu3 <duggasanianusha3@gmail.com>
2025-02-12 16:20:48 -0600

    Initial commit

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../models/appointment';
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css'],
+})
+export class AppointmentListComponent implements OnInit {
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
+  ngOnInit(): void {
+    console.log('got loaded');
+    let savedApptmts = localStorage.getItem('appointments');
+    this.appointments = savedApptmts ? JSON.parse(savedApptmts) : [];
+  }
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newApptmt: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+      };
+      this.appointments.push(newApptmt);
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
+    }
+  }
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
+  }
+}
